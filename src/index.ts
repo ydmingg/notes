cb8fef06be008069ec6c518fb2442a60715cb95b
xmyy <aaabbb@xm.funxdata.com>
2023-09-22 17:51:17 +0800

    update layout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-{ }
+let app = document.querySelector("#app") as HTMLElement;
+app.style.height = "100vh"
+app.className = "row align-center justify-center"
+
+let h1 = document.createElement("h1");
+h1.textContent = "Welcome To Home"
+
+app.appendChild(h1)
